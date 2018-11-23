2887e6c8932553eff16d5f0694f68802d38f8c45
Lucas Duete <lm.duete@gmail.com>
2018-11-22 21:18:25 -0300

    Implementando questao 03

---
+++ questao3/src/main/java/io/github/lucasduete/sd/atividade/estilosEsockets/node1.java
@@ -0,0 +1,33 @@
+package io.github.lucasduete.sd.atividade.estilosEsockets;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class node1 {
+
+    public static void main(String[] args) {
+        try (Socket client = new Socket("127.0.0.1", 33803)) {
+
+            Map<String, Integer> data = new HashMap<>();
+
+            data.put("x", 7);
+            data.put("y", 13);
+            data.put("node", 1);
+
+            ObjectOutputStream outputStream = new ObjectOutputStream(client.getOutputStream());
+            outputStream.writeObject(data);
+
+            ObjectInputStream inputStream = new ObjectInputStream(client.getInputStream());
+            Integer result = inputStream.readInt();
+
+            System.out.println("O resultado é: " + result);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
